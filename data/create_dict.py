259507dc4f6204f5c6ff3b47f8bf56a4a4a619aa
soazig <soazig@users.noreply.github.com>
2015-11-03 01:24:03 -0800

    Revert "added Makefile to verify the data (downloading the tgz file will take…"

--- data/create_dict.py
+++
@@ -1,7 +0,0 @@
-import 
-
-def create_dict(
-for line in f:
-        listedline = line.strip().split('=') # split around the = sign
-            if len(listedline) > 1: # we have the = sign in there
-                        newDict[listedline[0]] = listedline[1]
