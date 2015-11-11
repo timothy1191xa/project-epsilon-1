d077c65a6b094aab76d6625d3fbe9727e3d4900c
Timothy Yu <macbookpro@airbears2-10-142-145-209.airbears2.1918.berkeley.edu>
2015-11-10 17:58:35 -0800

    Updated organize_behavior_data.py

--- code/utils/organize_behavior_data.py
+++ code/utils/organize_behavior_data.py
@@ -14,3 +14,6 @@ behav3=np.loadtxt(data_location+'sub001/behav/task001_run001/behavdata.txt',skip
 behav_total_run=np.concatenate((behav1,behav2,behav3),axis=0)
 
 """delete the rows that contain -1 in respcat (these are errors in experiment so we should take them out"""
+behav_total_run=np.delete(behav_total_run, behav_total_run[:,5]==-1,axis=0)		
+		
+#your behav_total_run is ready to use!
