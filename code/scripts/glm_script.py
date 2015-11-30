2b4aabaccda5c90ff8b7bc19f8808e2b60b2b790
lizhua <lizhua@berkeley.edu>
2015-11-30 02:38:34 -0800

    change plot location

--- code/scripts/glm_script.py
+++ code/scripts/glm_script.py
@@ -6,20 +6,21 @@ from convolution_normal_script import X_matrix
 import nibabel as nib
 import matplotlib.pyplot as plt
 
-# run the convolution_normal_script.py script to get the design matrix X_matrix
+
+location_of_plot = "../../plots/"
+location_of_data = "../../data/ds005/sub001/BOLD/task001_run001/"
 
 #get 4_d image data
-data_location = "../../data/ds005/sub001/BOLD/task001_run001/"
-img = nib.load(data_location + "bold.nii")
+img = nib.load(location_of_data+ "bold.nii")
 data = img.get_data()
 
 beta_4d = glm_multi(data[...,4:],X_matrix)
 MRSS, fitted, residuals = glm_diagnostics(beta_4d, X_matrix, data[...,4:])
 
 print ("MRSS of multiple regression: "+str(np.mean(MRSS)))
-plt.plot(data[32,32,17], label = "actual")
-plt.plot(fitted[32,32,17], label = "fitted")
-plt.title("Subject 001, voxel (32,32,17) actual vs fitted")
+plt.plot(data[4,22,11], label = "actual")
+plt.plot(fitted[4,22,11], label = "fitted")
+plt.title("Subject 001, voxel (4,22,11) actual vs fitted")
 plt.legend(loc = "upper left", fontsize = "smaller")
-plt.savefig("glm_fitted.png")
+plt.savefig(location_of_plot + "glm_fitted.png")
 plt.close()
