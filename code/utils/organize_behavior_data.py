a289804ce87f4731f68136e48617750885db31ff
Min Gu Jo <mingu08@berkeley.edu>
2015-12-07 18:03:13 -0800

    updated logistic regression, so it's good to plot the betas

--- code/utils/organize_behavior_data.py
+++ code/utils/organize_behavior_data.py
@@ -1,3 +1,4 @@
+
 """
 Purpose:
 -----------------------------------------------------------------------------------
@@ -30,9 +31,9 @@ def load_behav_txt(subject_number):
     """
 
 	#load texts
-	behav1=np.loadtxt(data_location+'sub00%s/behav/task001_run001/behavdata.txt'%(subject_number),skiprows=1)
-	behav2=np.loadtxt(data_location+'sub00%s/behav/task001_run002/behavdata.txt'%(subject_number),skiprows=1)
-	behav3=np.loadtxt(data_location+'sub00%s/behav/task001_run003/behavdata.txt'%(subject_number),skiprows=1)
+	behav1=np.loadtxt(data_location+'sub'+str(subject_number).zfill(3)+'/behav/task001_run001/behavdata.txt',skiprows=1)
+	behav2=np.loadtxt(data_location+'sub'+str(subject_number).zfill(3)+'/behav/task001_run002/behavdata.txt',skiprows=1)
+	behav3=np.loadtxt(data_location+'sub'+str(subject_number).zfill(3)+'/behav/task001_run003/behavdata.txt',skiprows=1)
 	
 	#concatenate them to be 1
 	behav_total_run=np.concatenate((behav1,behav2,behav3),axis=0)
@@ -62,7 +63,7 @@ def load_behav_text_one(subject_number, run_number):
         
     """
 
-	behav=np.loadtxt(data_location+'sub00%s/behav/task001_run00%s/behavdata.txt'%(subject_number,run_number),
+	behav=np.loadtxt(data_location+'sub'+str(subject_number).zfill(3)+'/behav/task001_run00%s/behavdata.txt'%(run_number),
 		skiprows=1)
 
 	#delete the rows that contain -1 in respcat (these are errors in experiment so we should take them out
@@ -89,54 +90,15 @@ def load_in_dataframe(subject_number):
     """
 
 	#convert 3 behavior datas in 1 subject into data frames.
-	run1 = pd.read_table(data_location+'sub00%s/behav/task001_run001/behavdata.txt'%(subject_number))
-	run2 = pd.read_table(data_location+'sub00%s/behav/task001_run002/behavdata.txt'%(subject_number))
-	run3 = pd.read_table(data_location+'sub00%s/behav/task001_run003/behavdata.txt'%(subject_number))
+	run1 = pd.read_table(data_location+'sub'+str(subject_number).zfill(3)+'/behav/task001_run001/behavdata.txt')
+	run2 = pd.read_table(data_location+'sub'+str(subject_number).zfill(3)+'/behav/task001_run002/behavdata.txt')
+	run3 = pd.read_table(data_location+'sub'+str(subject_number).zfill(3)+'/behav/task001_run003/behavdata.txt')
 
 	#append all the runs in one pandas data frame
 	r=run1.append(run2)
 	run_total=r.append(run3) 
 	
 	return run_total
-
-def load_model_one(subject_number, run_number):
-	""" Return the model (conditions) of subject's single run
-
-    Parameters
-    ----------
-    subject_number : int
-    	Subject Number
-    run_number : int
-    	Run Number	
-    
-    Returns
-    -------
-    task : np.array
-    	the model (conditions) of subject's single run 
-    gain : np.array
-    	the model (conditions) of subject's single run
-    loss : np.array
-    	the model (conditions) of subject's single run
-    dist : np.array
-    	the model (conditions) of subject's single run
-        
-    """
-
-	task=np.loadtxt(data_location+'sub00%s/model/model001/onsets/task001_run00%s/cond001.txt'%(subject_number,run_number),
-		skiprows=1)
-	gain=np.loadtxt(data_location+'sub00%s/model/model001/onsets/task001_run00%s/cond002.txt'%(subject_number,run_number),
-		skiprows=1)
-	loss=np.loadtxt(data_location+'sub00%s/model/model001/onsets/task001_run00%s/cond003.txt'%(subject_number,run_number),
-		skiprows=1)
-	dist=np.loadtxt(data_location+'sub00%s/model/model001/onsets/task001_run00%s/cond004.txt'%(subject_number,run_number),
-		skiprows=1)
-
-	#delete the rows that contain -1 in respcat (these are errors in experiment so we should take them out
-
-	return task, gain, loss, dist
-
-
-
 
 
 
