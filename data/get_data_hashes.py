b0ac14fbb3e52b554884249d53a0c5789364373d
soazig <soazig.kaam@berkeley.edu>
2015-12-09 22:18:10 -0800

    Modify function to get the hashes for each file in a directory and dubdirectories

---
+++ data/get_data_hashes.py
@@ -0,0 +1,16 @@
+"""
+Creates a .json file with hashes for the testing data ds114
+and the filtered data ds005_filtered containing file names 
+and corresponding hashes.
+"""
+from __future__ import print_function
+from data import * 
+import json
+import pdb
+
+if __name__ == "__main__":
+    dirs_ls = ['ds114','ds005_filtered']
+    for d in dirs_ls:
+        d_dict = generate_dir_md5(d)
+        with open(d + '_hashes.json', 'w') as file_out:
+            json.dump(d_dict, file_out)
