f9173e1a70f525113397bc831f80ed3be315ba7f
soazig <soazig.kaam@berkeley.edu>
2015-12-10 00:43:22 -0800

    added a test on download file and get the hash

--- data/tests/test_data.py
+++ data/tests/test_data.py
@@ -3,11 +3,11 @@ Run with:
     nosetests test_data.py
 """
 
-
 from __future__ import print_function
 from .. import data 
 
 import tempfile
+import urllib
 
 def test_check_hashes():
     with tempfile.NamedTemporaryFile() as temp:
@@ -18,3 +18,15 @@ def test_check_hashes():
         assert data.check_hashes(d)
         d = {fname: "4b82f8bf4df2bfb0e66ccaa7306fd024"}
         assert not data.check_hashes(d)
+
+def test_get_hashes():
+    #Download json file senators-list.json and store it in the current directory
+    link = 'http://jarrodmillman.com/rcsds/data/senators-list.json'
+    urllib.urlretrieve(link, 'senators-list.json')
+    dir_hashes = data.generate_dir_md5('.')
+    #Verify the hash for senators-list.json and remove the file
+    assert not dir_hashes['./senators-list.json'] == \
+              'f0c1a76b571ab86968329a7b202f2edf'
+    assert dir_hashes['./senators-list.json'] == \
+              'd96aaf10750b3f44303dd055d7868b2d'
+    os.remove('senators-list.json')
