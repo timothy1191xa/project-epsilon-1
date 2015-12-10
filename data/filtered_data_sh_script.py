f44c82db081f338bb6206ea2baa857817f1fbb4e
soazig <soazig.kaam@gmail.com>
2015-12-09 17:14:45 -0800

    fixed bug in filename

--- data/filtered_data_sh_script.py
+++ data/filtered_data_sh_script.py
@@ -26,7 +26,7 @@ for s in subject_list:
     for r in run_list:
         wget = 'wget --no-check-certificate -P'
         location = data_path + '%s_filtered/sub'%(project_id) + s.zfill(3)\
-           +'/BOLD/task001_run%s/'%(r.zfill(3)) + 'bold_filtered.nii.gz '
+           +'/BOLD/task001_run%s/'%(r.zfill(3))
         link = base_url + 'sub%s/model/model001/task001_run' %(s.zfill(3))\
                + r.zfill(3) + '.feat/' + file_name
         txt_out.append((wget, location, link))
