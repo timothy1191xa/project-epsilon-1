709bf6c5b7d2c23a6bf27799817f1fd95df8662e
soazig <soazig.kaam@gmail.com>
2015-12-09 19:23:24 -0800

    modified the script for the download files to be overriden with a new download (in case error in download for example

--- data/filtered_data_sh_script.py
+++ data/filtered_data_sh_script.py
@@ -22,14 +22,18 @@ file_name = 'filtered_func_data_mni.nii.gz'
 
 #Write local downloading location and link on txt file
 txt_out = []
+mkdir_out = []
 for s in subject_list:
     for r in run_list:
-        wget = 'wget --no-check-certificate -P'
+        wget = 'wget --no-check-certificate '
         location = data_path + '%s_filtered/sub'%(project_id) + s.zfill(3)\
            +'/BOLD/task001_run%s/'%(r.zfill(3))
         link = base_url + 'sub%s/model/model001/task001_run' %(s.zfill(3))\
                + r.zfill(3) + '.feat/' + file_name
-        txt_out.append((wget, location, link))
+        txt_out.append((wget + link + ' -O ' + location + 'bold_filtered.nii.gz'))
+        mkdir_out.append(('mkdir -p ' + location))
 
 np_txt_out = np.array((txt_out))
-np.savetxt(data_path + 'dwn_filtered_data_script.sh', np_txt_out, delimiter=" ", fmt="%s")
+np_mkdir_out = np.array((mkdir_out))
+np_out = np.vstack((np_mkdir_out,np_txt_out)).ravel([-1])
+np.savetxt(data_path + 'dwn_filtered_data_script.sh', np_out, delimiter=" ", fmt="%s")
