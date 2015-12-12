e298fd75be274e028e07e6100a5083b95fd231e9
Min Gu Jo <mingu08@berkeley.edu>
2015-12-12 14:14:07 -0800

    start to fix the log reg script. plot for whole subjects

--- code/utils/scripts/log_regression_script.py
+++ code/utils/scripts/log_regression_script.py
@@ -1,8 +1,25 @@
-""" Linear Regression on Begavioral data """
+"""
+Purpose:
+-----------------------------------------------------------------------------------
+We try to capture the significance of gain and loss amount condition for each subjects.
+We fit the logistic regression line based on their responses on the experiment. The slope 
+of the fitted line illustrates the subject's sensitivity on either gain or loss amount. 
 
-import sys
-sys.path.append(".././utils")
+This script outputs plots for each subject and combine them into one image of subplots. 
+-----------------------------------------------------------------------------------
+Step:
+1. 
+"""
+
+from __future__ import absolute_import, division, print_function
+import sys, os
+#TODO : later change this
+sys.path.append(os.path.join(os.path.dirname(__file__), "../"))
+import numpy as np
+import matplotlib.pyplot as plt
+import nibabel as nib
 from logistic_reg import *
+from organize_behavior_data import *
 
 
 a=add_gainlossratio(behav_df)
