70d5c5181c95b22fc006e1326e91eb13f4103334
Min Gu Jo <mingu08@berkeley.edu>
2015-12-12 14:15:11 -0800

    save before change. start the for loop.

--- code/utils/scripts/log_regression_script.py
+++ code/utils/scripts/log_regression_script.py
@@ -22,6 +22,18 @@ from logistic_reg import *
 from organize_behavior_data import *
 
 
-a=add_gainlossratio(behav_df)
-b=organize_columns(a)
-log_regression(b)
+
+# Create the necessary directories if they do not exist
+dirs = ['../../../fig','../../../fig/log_reg_behav']
+for d in dirs:
+    if not os.path.exists(d):
+            os.makedirs(d)
+
+# Locate the different paths
+#TODO: the current location for this file project-epsilon/code/scripts
+project_path = '../../../'
+# TODO: change it to relevant path
+data_path = project_path+'data/ds005/'
+subject_list = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16']
+
+images_paths = ['ds005_sub' + s.zfill(3) +'_log_reg_behav' for s in subject_list]
