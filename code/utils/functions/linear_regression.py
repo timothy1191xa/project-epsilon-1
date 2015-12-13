0c9a0a7735e1c057dde3360cb00822b4ab85cedc
Timothy Yu <macbookpro@airbears2-10-142-18-68.airbears2.1918.berkeley.edu>
2015-12-13 15:55:54 -0800

    rewrite functions for linear regression

--- code/utils/functions/linear_regression.py
+++ code/utils/functions/linear_regression.py
@@ -19,7 +19,6 @@ from logistic_reg import *
 
 
 
-
 """
 Parameters:
 
@@ -34,19 +33,13 @@ Return:
 def load_data(subject, data_dir = "/Users/macbookpro/Desktop/stat159_Project/"):
 	
 	# Get the directory where data is stored
-	data_location= data_dir + 'ds005/sub' + subject
+	data_location = data_dir + 'ds005/sub' + subject
 
-	try:
+	# Load the data for each run
+	run1 = pd.read_table(data_location+"/behav/task001_run001/behavdata.txt")
+	run2 = pd.read_table(data_location+"/behav/task001_run002/behavdata.txt")
+	run3 = pd.read_table(data_location+"/behav/task001_run003/behavdata.txt")
 
-		# Load the data for each run
-		run1 = pd.read_table(data_location+"/behav/task001_run001/behavdata.txt")
-		run2 = pd.read_table(data_location+"/behav/task001_run002/behavdata.txt")
-		run3 = pd.read_table(data_location+"/behav/task001_run003/behavdata.txt")
-
-	except IOError:
-
-		print ("Can't find files in such directory! Please enter the directory where you store ds005 dataset!")
-		return
 
 	run_1=run1.append(run2)
 	run_total=run_1.append(run3) #append all the data frames of run
@@ -54,83 +47,9 @@ def load_data(subject, data_dir = "/Users/macbookpro/Desktop/stat159_Project/"):
 	return(run_total)
 
 
-"""
-To combine all the behavioral data
-
-Parameters:
-
-	data_dir: The working directory that you store your data
-
-Return:
-	
-	all_data: the behavioral data that contains 16 subjects, each subject has 3 runs
 
 """
-def combine_all_data(data_dir = "/Users/macbookpro/Desktop/stat159_Project/"):
-
-	# Get all the subjects
-	all_subjects = ['001', '002', '003', '004', '005', '006', '007', 
-	'008', '009', '010', '011', '012', '013', '014', '015', '016']
-
-	all_data = []
-
-	# Loop for 16 subjects to get the data
-	for i in all_subjects:
-		temp = load_data(i, data_dir)
-		
-		# if there's no such dataset, then stop running the loop and return nothing
-		if temp is None:
-			return
-
-		# combine all the data
-		all_data.append(temp)
-
-	# Concat the data
-	all_data = pd.concat(all_data)
-
-	# Calculate the difference for future use
-	all_data['diff'] = all_data['gain'] - all_data['loss']
-
-	# Calculate the ratio for future use
-	all_data['ratio'] = all_data['gain'] / all_data['loss']
-
-
-	all_data['log_gain'] = np.log(all_data['gain'])
-
-	all_data['log_loss'] = np.log(all_data['loss'])
-
-	all_data['log_diff'] = np.log(all_data['diff'])
-
-	all_data['log_ratio'] = np.log(all_data['ratio'])
-
-	# # Remove the rows that have respcat == -1 
-	#(meaning that the individual chooses to not participate the activity)
-	all_data = all_data[np.logical_not(all_data['respcat'] == -1)]
-
-	return (all_data)
-
-
-
-
-def load_each_subject(data_dir = "/Users/macbookpro/Desktop/stat159_Project/"):
-
-	all_subjects = ['001', '002', '003', '004', '005', '006', '007', 
-	'008', '009', '010', '011', '012', '013', '014', '015', '016']
-
-	l = []
-
-	for i in all_subjects:
-		l.append(load_data(i, data_dir))
-
-	for i in range(len(all_subjects)):
-		l[i]['ratio'] = l[i]['gain'] / l[i]['loss']
-
-	return l
-
-
-
-"""
-To combine all the behavioral data
+To perform linear regression
 
 Parameters:
 
@@ -239,20 +158,6 @@ def linear_regression_RT_with_gain_and_loss(data):
 """
 
 
-
-def linear_regression_fast(data, formula):
-	#plt.plot(data['diff'], data['RT'], '+')
-	# It seems like there's no siginificant relationship between diff and Response time
-	# When we do the regression, it shows that difference between gain and loss does not really matter either.
-
-
-	est = smf.ols(formula = formula, data=data).fit()
-	est.summary()
-
-	return est
-
-
-
 def simple_regression_plot(data, dep_var, exp_var):
 	y = data[dep_var]
 	x = data[exp_var]
@@ -274,27 +179,7 @@ def simple_regression_plot(data, dep_var, exp_var):
 
 
 
-def beta_statistics():
-
-	data = load_each_subject()
-
-	betas = [0] * len(data)
-	pvalues = [0] * len(data)
-
-	for i in range(len(data)):
-		betas[i], pvalues[i] = linear_regression(data[i], 'RT', 'gain', 'loss')
-
-	lambdas = []
-
-	for i in range(len(data)):
-		lambdas.append( math.log(betas[i][2] / betas[i][1])    )
-
-	return
-
-def plot_neural_and_behav_loss_aversion(data, beta = None):
-
-	all_subjects = ['001', '002', '003', '004', '005', '006', '007', 
-	'008', '009', '010', '011', '012', '013', '014', '015', '016']
+def plot_neural_and_behav_loss_aversion(all_subjects, data, beta = None):
 
 	lambdas = []
 	loss_aversion = []
